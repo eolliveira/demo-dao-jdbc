31356710d16a106a6aa3ad712ecf2d3f4585f551
Erick Oliveira <erick.oliveira9607@gmail.com>
2021-05-26 00:18:50 -0300

    findById Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDAO;
 import model.dao.SellerDAO;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,6 +14,13 @@ public class Program {
 		Seller seller = sd.findById(3);
 		
 		System.out.println(seller);
+		
+		
+		DepartmentDAO depDAO = DaoFactory.createDepartmentDao();
+		Department dep = depDAO.findById(2);
+		
+		System.out.println(dep);
+		
 	}
 
 }
--- src/model/entities/impl/DepartmentDaoJdbc.java
+++ src/model/entities/impl/DepartmentDaoJdbc.java
@@ -3,11 +3,13 @@ package model.entities.impl;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDAO;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class DepartmentDaoJdbc implements DepartmentDAO {
 	
@@ -35,9 +37,42 @@ public class DepartmentDaoJdbc implements DepartmentDAO {
 		
 	}
 
+	//busca departamento pelo Id
 	@Override
 	public Department findById(Integer id) {
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps = conn.prepareStatement("SELECT \r\n" + 
+					"	d.*\r\n" + 
+					"FROM \r\n" + 
+					"	department d\r\n" + 
+					"WHERE d.Id = ?");
+			ps.setInt(1, id);
+			rs = ps.executeQuery();
+			
+			//se retornar um valor na consulta
+			if(rs.next()) {
+				Department dep = instantiateDepartment(rs);
+				
+				return dep;
+			}
+		} catch (Exception e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(ps);
+			DB.closeResultSEt(rs);
+		}
+		
 		return null;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("Id"));
+		dep.setName(rs.getString("Name"));
+		
+		return dep;
 	}
 
 	@Override
